2f9ee4582c160e297188e1b33120e676736062de
Dylan <locducnguyen2001@gmail.com>
2025-12-25 18:16:59 +0700

    cicd: upgrade netclient and netmaker to 1.4.0

--- nexmaker/mix.exs
+++ nexmaker/mix.exs
@@ -4,7 +4,7 @@ defmodule Nexmaker.MixProject do
   def project do
     [
       app: :nexmaker,
-      version: "1.2.0",
+      version: "1.4.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()

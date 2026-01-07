55ea7548fc34e75257a4b3136711ddfe2e7a50d6
Dylan <locducnguyen2001@gmail.com>
2026-01-07 10:06:29 +0700

    chore(admin): remove all old tests

--- edge_admin/test/edge_admin/gettext_interpolation_test.exs
+++
@@ -1,6 +0,0 @@
-# edge_admin/test/edge_admin/gettext_interpolation_test.exs
-defmodule EdgeAdmin.GettextInterpolationTest do
-  use ExUnit.Case, async: true
-
-  doctest EdgeAdmin.GettextInterpolation, import: true
-end

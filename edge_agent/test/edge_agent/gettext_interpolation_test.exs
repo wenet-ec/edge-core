d47e068cb21f50bfa987aae7c9e73084c80f57b5
Dylan <locducnguyen2001@gmail.com>
2026-02-12 18:21:00 +0700

    chore(agent): remove old tests

--- edge_agent/test/edge_agent/gettext_interpolation_test.exs
+++
@@ -1,6 +0,0 @@
-# edge_agent/test/edge_agent/gettext_interpolation_test.exs
-defmodule EdgeAgent.GettextInterpolationTest do
-  use ExUnit.Case, async: true
-
-  doctest EdgeAgent.GettextInterpolation, import: true
-end

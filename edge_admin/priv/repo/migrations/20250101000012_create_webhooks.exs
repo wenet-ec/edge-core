df7aaa4fb8f94c6ef751ea4963a5e8c8ecf3c8eb
Dylan <locducnguyen2001@gmail.com>
2026-06-01 09:00:34 +0700

    fix(admin): change the webhook url type from string/char to string/text

--- edge_admin/priv/repo/migrations/20250101000012_create_webhooks.exs
+++ edge_admin/priv/repo/migrations/20250101000012_create_webhooks.exs
@@ -5,7 +5,7 @@ defmodule EdgeAdmin.Repo.Migrations.CreateWebhooks do
   def change do
     create table(:webhooks, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :url, :string, null: false
+      add :url, :text, null: false
       add :secret, :binary, null: false
       add :headers, :binary
       add :subscribed_events, {:array, :string}, null: false, default: []
